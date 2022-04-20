1460edb607d51c9011f669d72151e7717393c0ea
John Lee <jle432edu@gmail.com>
2022-04-19 20:26:02 -0700

    Updat database connection method - Update db.js file to the original connection method to a Postgres database to ensure database connection is made when running app locally and all tests pass when attempting to merge with main.

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,8 @@ const Sequelize = require("sequelize");
 require("dotenv").config();
 const { PG_PASSWORD } = process.env;
 
-const db = new Sequelize('messenger', 'postgres', PG_PASSWORD, {
-  host: 'localhost',
-  dialect: 'postgres'
+const db = new Sequelize(process.env.DATABASE_URL || `postgres://postgres:${PG_PASSWORD}@localhost:5432/messenger`, {
+  logging: false
 });
 
 module.exports = db;
